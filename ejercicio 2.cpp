d156777fa6033e7d93cd133eeaeb8f4183ad0079
Mencos14 <164411628+Mencos14@users.noreply.github.com>
2024-05-06 14:01:46 -0600

    Add files via upload

---
+++ ejercicio 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int i = 1;
+    while (i<12){
+        cout << i <<"x 12 ="<< i*12 << endl;
+        i++;
+    }
+return 0;     
+}
+
+    
